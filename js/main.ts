7c27ad448cf7325b5a51fa3717eebe900b1d1cc4
Leonardo Alves <leunardosevla@gmail.com>
2017-07-23 16:43:37 -0300

    feat: added showdown and load file function

--- js/main.ts
+++ js/main.ts
@@ -1,8 +1,25 @@
-import { showAbout } from './about';
-import { showContact } from './contact';
+import { get } from 'http';
+import { Converter } from 'showdown';
+import { loadAbout, showAbout } from './about';
+import { loadContact, showContact } from './contact';
 
+function loadFile(file: string, callback: (body: string) => void) {
+    const rawFile = new XMLHttpRequest();
+    const text: string = 'e';
+    get(file, (r) => {
+        let body = '';
+        r.on('data', (d): void => { body += d; });
+        r.on('end', () => { callback(body); });
+    });
+}
+const converter = new Converter();
 const contactAnchor = document.getElementById('contact-anchor');
 const aboutAnchor = document.getElementById('about-anchor');
 
 contactAnchor.addEventListener('click', showContact);
+contactAnchor.addEventListener('click', loadContact);
+
 aboutAnchor.addEventListener('click', showAbout);
+aboutAnchor.addEventListener('click', loadAbout);
+
+export { loadFile, aboutAnchor, contactAnchor, converter };
